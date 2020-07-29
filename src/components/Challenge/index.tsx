c17033c37628c7a105e767f7abc2c8923f186d03
cfchenr <c.henriques@ua.pt>
2020-07-29 12:24:21 +0100

    Add button to login

--- src/components/Challenge/index.tsx
+++ src/components/Challenge/index.tsx
@@ -7,12 +7,42 @@ import ChallengeInfo from '../ChallengeInfo'
 import ContestList from '../ContestList'
 
 import Drop from '../Drop'
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
+
+import { GoogleLogin } from 'react-google-login'
+import { BtnGoogle } from '../Authentication/styles'
 
 const Challenge: React.FC = () => {
   const data = useSelector((state: any) => state.data)
   const authentication = data.auth.authenticated
+  const dataChallenge = data.challenge
   const challengeName = data.challenge.name
+
+  const dispatch = useDispatch()
+
+  const login = (response: any) => {
+    const newAuth = {
+      data: {
+        auth: {
+          authenticated: true,
+          user: {
+            id: response.googleId,
+            name: response.profileObj.givenName,
+            surname: response.profileObj.familyName,
+            image: response.profileObj.imageUrl
+          },
+          token: response.tokenId
+        },
+        challenge: dataChallenge
+      }
+    }
+    dispatch({ type: 'LOGIN', data: newAuth })
+  }
+
+  const badResponseGoogle = (response: any) => {
+    console.log(response)
+    dispatch({ type: 'BAD_RESPONSE', data: null })
+  }
 
   return (
     <Container>
@@ -27,7 +57,17 @@ const Challenge: React.FC = () => {
               <Message>
                 <Row>
                   <Col xs={12} className="text-center">
-                    <LoginInfo>You need to log in to send your solution!</LoginInfo>
+                    <LoginInfo>You need to <GoogleLogin
+                      clientId='156221636932-bvl7ocr3bhrkikgcqc99k4g1a1s0sla1.apps.googleusercontent.com'
+                      buttonText='Login'
+                      onSuccess={login}
+                      onFailure={badResponseGoogle}
+                      cookiePolicy={'single_host_origin'}
+                      isSignedIn={true}
+                      render={renderProps => (
+                        <BtnGoogle onClick={renderProps.onClick} disabled={renderProps.disabled}>Sign In w/ Google</BtnGoogle>
+                      )}
+                    /> to send your solution!</LoginInfo>
                   </Col>
                 </Row>
               </Message>
