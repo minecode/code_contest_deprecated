94c5be18fc2805401c72de9f55cc2556b7fb892d
minecodebot <minecode.geral@gmail.com>
2020-09-14 13:36:57 +0000

    deploy: minecode/code_contest_frontend@b1a9cdbd4e2c10c6d133539eaee23ec193a38753

---
+++ _next/static/Ki1EqR34Q5CxsNNMnGjfo/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002F8a28b14e.5592ca5ffefd7699e1c0.js","static\u002Fchunks\u002Fpages\u002Findex-fc053c4481b67f5052f4.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-21598c12abce689a38e8.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
